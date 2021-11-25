05df1b8ac914a00594b54a1b2cd64242fd9ee3d9
NgKarKai <clhskkng6771t9@gmail.com>
2021-11-25 12:22:40 +0800

    Established database connection, created UserDB.cs for database operations

---
+++ Database/DBController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Finexus_Hackathon.Database
+{
+    public class DBController
+    {
+        protected SqlConnection conn;
+        //Establish database connection
+        protected void connectDB()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["finexus"].ConnectionString);
+            conn.Open();
+        }
+
+        //Disconnect connection
+        protected void disconnectDB()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+}
---
+++ Database/UserDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Finexus_Hackathon.Database;
+
+namespace Finexus_Hackathon.Database 
+{
+    public class UserDB : DBController
+    {
+        //Init and establish database connection
+        public UserDB()
+        {
+            connectDB();
+        }
+    }
+}
