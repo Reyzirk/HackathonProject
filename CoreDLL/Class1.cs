7dba899ae23f1043d8db1b101f1e5db11d5a3e36
ChooMeng <pohchoomeng01@gmail.com>
2021-11-28 21:07:25 +0800

    Fixed CoreDLL

---
+++ CoreDLL/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+
+namespace CoreDLL
+{
+    public class CoreDLL
+    {
+        public static String hashPassword(String input, String salt)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input + salt);
+            SHA256Managed hashString = new SHA256Managed();
+            byte[] hash = hashString.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public static string generateSalt()
+        {
+            var random = new RNGCryptoServiceProvider();
+
+            //Max salt length
+            int max_length = 32;
+
+            //Empty salt array
+            byte[] salt = new byte[max_length];
+
+            //Build random bytes
+            random.GetNonZeroBytes(salt);
+
+            //return the encoded salt
+            return Convert.ToBase64String(salt);
+        }
+        private static Boolean validCard(String input)
+        {
+            input = input.Replace("-", "");
+            int[] card = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                card[i] = (int)(input[i] - '0'); //In ASCII, digit number is not 0-9
+            }
+            for (int i = input.Length - 2; i >= 0; i -= 2)
+            {
+                int temp = card[i];//Store the value in the variable to process in future, to prevent interrupt original number
+                temp = temp * 2;
+                if (temp > 9)
+                {
+                    temp = temp % 10 + 1;
+                }
+                card[i] = temp;
+            }
+            int total = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                total += card[i];
+            }
+            //Valid credit number will contains 0 remainder
+            if (total % 10 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /* Check Credit Card number whether following "xxxx-xxxx-xxxx-xxxx"
+         * valid = return true
+         * invalid = return false
+         */
+        public static Boolean checkCardNumber(String input)
+        {
+            Regex rgx = new Regex(@"[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}");
+            if (rgx.IsMatch(input))
+            {
+                if (validCard(input))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Boolean checkEmail(String input)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
